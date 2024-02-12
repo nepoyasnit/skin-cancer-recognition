5c0a87bb06ac0b4c6a933d81e283b3e1cccaf341
nepoyasnit <maksiksay@gmail.com>
2024-02-12 14:57:10 +0300

    remove unuseful libs

--- transformers/efficientvit-isic2019mod.py
+++ transformers/efficientvit-isic2019mod.py
@@ -1,16 +1,10 @@
-import torch
-
 from base.model import Model
 from base.evaluate import evaluate_model
 from base.train import _run
 
 
 if __name__ == "__main__":
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
     model = Model('efficientvit_m5.r224_in1k',pretrained=True)
-    model.to(device)
 
     for i in range(1):
         _run(task='2019', model_name='efficientvit2019', fold=i, model=model)
