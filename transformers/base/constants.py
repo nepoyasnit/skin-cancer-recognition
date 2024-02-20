fix normalization on whole dataset

transformers/base/constants.py
@@ -7,7 +7,7 @@ IMG_SIZE = 224
 
 BATCH_SIZE = 277 # optimal by formula (gpu_mem - model_size) / (forw_backw_size)
 
-LR = 1e-4
+LR = 3e-05
 
 ALPHA2019 = 0.2 # because of nevus distribution
 
@@ -29,6 +29,7 @@ _mean = np.array([0.6237459654304592, 0.5201169854503829, 0.5039494477029685])
 
 _std = np.array([0.24196317678786788, 0.2233599432947672, 0.23118716487089888])
 
+IMG_FORMAT = '.jpg'
 
 EPOCH_WEIGHTS_PATH = 'weights/checkpoints/%(model_name)s/%(epoch)s_%(datetime)s.pth'
 

transformers/base/load_data.py
@@ -1,14 +1,16 @@
 import os
+import torch
 import numpy as np
 import pandas as pd
+import torchvision.transforms as transforms
 from sklearn.model_selection import StratifiedKFold
 from sklearn.utils.class_weight import compute_class_weight
 
-from .transforms import get_transforms
+from .transforms import get_train_transforms, get_valid_transforms, get_base_transforms
 from .dataset import ClassificationDataset
-from .utils import compute_class_dist
+from .utils import compute_class_dist, get_mean_and_std
 from .constants import TRAIN_IMG_PATH2019, TRAIN_IMG_PATH2020, TRAIN_LABELS_PATH2019, TRAIN_LABELS_PATH2020, \
-                    TRAIN_LABELS_PATH_BALANCED, RANDOM_SEED, IMG_SIZE, _mean, _std
+                    TRAIN_LABELS_PATH_BALANCED, RANDOM_SEED, IMG_SIZE, BATCH_SIZE, IMG_FORMAT
 
 
 def load_isic_training_data(image_folder2019: str, labels_folder2019: str,
@@ -19,12 +21,12 @@ def load_isic_training_data(image_folder2019: str, labels_folder2019: str,
     known_category_names = list(data.columns.values[1:3])
     
     # Add path and category columns
-    data['path'] = data.apply(lambda row : os.path.join(image_folder2019, row['image_name']+'.jpg'), axis=1)
+    data['path'] = data.apply(lambda row : os.path.join(image_folder2019, row['image_name'] + IMG_FORMAT), axis=1)
     data['category'] = np.argmax(np.array(data.iloc[:,1:3]), axis=1)
 
     if labels_folder2020:
         data2020 = pd.read_csv(labels_folder2020)
-        data2020['path'] = data2020.apply(lambda row : os.path.join(image_folder2020, row['image_name']+'.jpg'), axis=1)
+        data2020['path'] = data2020.apply(lambda row : os.path.join(image_folder2020, row['image_name'] + IMG_FORMAT), axis=1)
         data2020['category'] = np.argmax(np.array(data2020.iloc[:,1:3]), axis=1)
 
         data = pd.concat([data, data2020]).reset_index()
@@ -44,12 +46,21 @@ def load_ph_test_data(image_folder: str, labels_file: str):
     test_images = test_df['path'].to_list()
     test_targets = test_df['category'].to_numpy()
 
-    _, valid_aug = get_transforms(IMG_SIZE, rgb_mean=_mean, rgb_std=_std)
+    base_aug = get_base_transforms(IMG_SIZE)
 
     test_dataset = ClassificationDataset(image_paths=test_images, 
                                          targets=test_targets,
                                          resize=[IMG_SIZE, IMG_SIZE],
-                                         augmentations=valid_aug)
+                                         augmentations=base_aug)
+    
+    loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                            batch_size=BATCH_SIZE,
+                                            drop_last=True, num_workers=2)
+    
+    test_mean, test_std = get_mean_and_std(loader)
+
+    test_dataset.augmentations = get_valid_transforms(IMG_SIZE, test_mean, test_std)
+
 
     return test_dataset
 
@@ -66,21 +77,34 @@ def get_train_val(labels: pd.DataFrame, fold: int):
     
     valid_images = df_valid['path'].to_list()
     valid_targets = df_valid['category'].to_numpy()
+
+    base_aug = get_base_transforms(IMG_SIZE)
+
+    whole_dataset = ClassificationDataset(
+        image_paths=labels['path'].to_list(),
+        targets=labels['category'].to_numpy(),
+        resize=[IMG_SIZE,IMG_SIZE],
+        augmentations=base_aug
+    )
+
+    whole_loader = torch.utils.data.DataLoader(dataset=whole_dataset,
+                                            batch_size=BATCH_SIZE,
+                                            drop_last=True, num_workers=2)
     
-    train_aug, valid_aug = get_transforms(IMG_SIZE, _mean, _std)
+    mean, std = get_mean_and_std(whole_loader)
 
     train_dataset = ClassificationDataset(
         image_paths=train_images,
         targets=train_targets,
         resize=[IMG_SIZE,IMG_SIZE],
-        augmentations=train_aug,
+        augmentations=get_train_transforms(IMG_SIZE, mean, std)
     )
     
     valid_dataset = ClassificationDataset(
         image_paths=valid_images,
         targets=valid_targets,
         resize=[IMG_SIZE,IMG_SIZE],
-        augmentations=valid_aug,
+        augmentations=get_valid_transforms(IMG_SIZE, mean, std)
     )
     
     return train_dataset, valid_dataset

transformers/base/model.py
@@ -8,7 +8,7 @@ from .constants import BETA
 
 
 class Model(nn.Module):
-    def __init__(self, timm_model_name: str, n_classes: int = 2, pretrained: bool = False):
+    def __init__(self, timm_model_name: str, n_classes: int = 2, pretrained: bool = True):
 
         super(Model, self).__init__()
         self.num_classes = n_classes

transformers/base/train.py
@@ -127,8 +127,8 @@ def _run(task: str, model_name: str, fold: int, model: Model):
     print('Device: ', device)
     model.to(device)
 
-    # optimizer = torch.optim.Adam(model.parameters(), lr=LR)   
-    optimizer = Lion(model.parameters(), lr=LR, weight_decay=WEIGHT_DECAY) 
+    optimizer = torch.optim.Adam(model.parameters(), lr=LR)   
+    # optimizer = Lion(model.parameters(), lr=LR, weight_decay=WEIGHT_DECAY) 
     #scheduler = StepLR(optimizer=optimizer, step_size=5, gamma=0.05)
 
     writer = SummaryWriter(comment=model_name)